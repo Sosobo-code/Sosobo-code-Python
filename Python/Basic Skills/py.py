bab2be6fc41062552b12e7ba992a00217e0ec72c
Sosobo-code <62372151+Sosobo-code@users.noreply.github.com>
2023-05-03 08:54:29 +1200

    Update py.py
    Paper Scissors rock

--- Python/Basic Skills/py.py
+++ Python/Basic Skills/py.py
@@ -1,49 +1,60 @@
 import random
-#this says hello
-print("Hello and welcome to rock paper scissors, you will play against an AI, choose either Rock Paper or Scissors.")
-player_score = 0
-computer_score = 0
-j = 0
-#this is the loop
-amountofrounds = int(input("How many rounds would you like to play?"))
-for i in range(amountofrounds):
-    player_choice = input("Enter your choice: ").lower()
 
+def play_round():
+    player_choice = input("""Enter your choice: rock paper or scissors
+    
+    """).lower()
     computer_choice = random.choice(['rock', 'paper', 'scissors'])
+    print(f"Computer chooses {computer_choice}.")
 
- 
     if player_choice == computer_choice:
-        print("It's a draw...")
-    elif player_choice == 'rock' and computer_choice == 'scissors':
-        print("You win this round! Rock beats scissors.")
-        player_score += 1
-    elif player_choice == 'paper' and computer_choice == 'rock':
-        print("You win this round! Paper beats rock.")
-        player_score += 1
-    elif player_choice == 'scissors' and computer_choice == 'paper':
-        print("You win this round! Scissors beat paper.")
-        player_score += 1
-    elif player_choice == "quit" or "q":
-        print("thanks for playing!")
-        break
-    elif player_choice == 'rock' and computer_choice == 'paper':
-        print("you lose this round")
-        computer_score += 1
-    elif player_choice == 'paper' and computer_choice == 'scissors':
-        print("you lose this round")
-        computer_score += 1
-    elif player_choice == 'scissors' and computer_choice == 'rock':
-        print("you lose this round")
-        computer_score += 1
+        print("It's a tie!")
+        return 0, 0
+    elif (player_choice == 'rock' and computer_choice == 'scissors') or \
+         (player_choice == 'paper' and computer_choice == 'rock') or \
+         (player_choice == 'scissors' and computer_choice == 'paper'):
+        print("You win this round!")
+        return 1, 0
     else:
-        print("You lose this round.")
-        computer_score += 1
+        print("You lose this round :(")
+        return 0, 1
 
+def play_game(rounds):
+    player_score = 0
+    computer_score = 0
+    for i in range(rounds):
+        print(f"\nRound {i+1}:")
+        p_score, c_score = play_round()
+        player_score += p_score
+        computer_score += c_score
+        print(f"Current score: Player - {player_score}, Computer - {computer_score}")
+    if player_score > computer_score:
+        print("Congratulations! You won the game!")
+    elif player_score < computer_score:
+        print("Sorry, you lost the game.")
+    else:
+        print("The game is tied.")
 
-    print(f"Your score: {player_score} | Computer score: {computer_score}")
+inf = 0
+def play_infinite_game():
+    player_score = 0
+    computer_score = 0
+    while inf < 1:
+        print("""New round:""")
+        p_score, c_score = play_round()
+        player_score += p_score
+        computer_score += c_score
+        print(f"Current score: Player - {player_score}, Computer - {computer_score}")
+        if input("Do you want to play again? (Y/N)").lower() == "n":
+            break
 
-    if player_score == amountofrounds:
-        print("Congratulations, you won!")
-        break
-    elif computer_score == amountofrounds:
-        print("Sorry, you lost.")
+# Introduction
+print("Welcome to Rock, Paper, Scissors!")
+mode = input("Select game mode: 1 - fixed number of rounds, 2 - infinite: ")
+if mode == "1":
+    rounds = int(input("Enter the number of rounds: "))
+    play_game(rounds)
+elif mode == "2":
+    play_infinite_game()
+else:
+    print("Invalid mode selected. Please choose either a fixed amount of rounds or infinite.")
